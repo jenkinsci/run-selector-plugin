21ff123a5b768a9ea8683075cf4e7b8027cc1a8f
Alexandru Somai <somai.alexandru@gmail.com>
2016-07-16 13:14:21 +0300

    [JENKINS-36734] Implement the runSelector Pipeline step

---
+++ src/main/java/org/jenkinsci/plugins/runselector/steps/RunSelectorExecution.java
@@ -0,0 +1,73 @@
+package org.jenkinsci.plugins.runselector.steps;
+
+import com.google.inject.Inject;
+import hudson.AbortException;
+import hudson.model.Job;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.runselector.Messages;
+import org.jenkinsci.plugins.runselector.RunFilter;
+import org.jenkinsci.plugins.runselector.RunSelector;
+import org.jenkinsci.plugins.runselector.context.RunSelectorContext;
+import org.jenkinsci.plugins.runselector.filters.NoRunFilter;
+import org.jenkinsci.plugins.runselector.selectors.StatusRunSelector;
+import org.jenkinsci.plugins.workflow.steps.AbstractSynchronousStepExecution;
+import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper;
+
+/**
+ * The execution of {@link RunSelectorStep}.
+ *
+ * @author Alexandru Somai
+ */
+public class RunSelectorExecution extends AbstractSynchronousStepExecution<RunWrapper> {
+
+    private static final RunSelector DEFAULT_RUN_SELECTOR = new StatusRunSelector();
+    private static final RunFilter DEFAULT_RUN_FILTER = new NoRunFilter();
+
+    @Inject
+    private transient RunSelectorStep step;
+
+    @StepContextParameter
+    private transient Run<?, ?> run;
+    @StepContextParameter
+    private transient TaskListener listener;
+
+    @Override
+    public RunWrapper run() throws Exception {
+
+        String projectName = step.getProjectName();
+        if (projectName == null) {
+            throw new AbortException(Messages.RunSelectorStep_MissingProjectName());
+        }
+
+        Jenkins jenkins = Jenkins.getActiveInstance();
+        Job<?, ?> upstreamJob = jenkins.getItem(projectName, run.getParent(), Job.class);
+        if (upstreamJob == null) {
+            throw new AbortException(Messages.RunSelectorStep_MissingProject(projectName));
+        }
+
+        RunSelector selector = step.getSelector();
+        if (selector == null) {
+            listener.getLogger().println(Messages.RunSelectorStep_MissingRunSelector(DEFAULT_RUN_FILTER.getDisplayName()));
+            selector = DEFAULT_RUN_SELECTOR;
+        }
+
+        RunFilter runFilter = step.getRunFilter();
+        if (runFilter == null) {
+            listener.getLogger().println(Messages.RunSelectorStep_MissingRunFilter());
+            runFilter = DEFAULT_RUN_FILTER;
+        }
+
+        RunSelectorContext context = new RunSelectorContext(jenkins, run, listener, runFilter);
+        context.setVerbose(step.isVerbose());
+
+        Run<?, ?> upstreamRun = selector.select(upstreamJob, context);
+        if (upstreamRun == null) {
+            throw new AbortException(Messages.RunSelectorStep_MissingRun(projectName, selector.getDisplayName(), runFilter.getDisplayName()));
+        }
+
+        return new RunWrapper(upstreamRun, false);
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/runselector/steps/RunSelectorStep.java
@@ -0,0 +1,90 @@
+package org.jenkinsci.plugins.runselector.steps;
+
+import hudson.Extension;
+import hudson.Util;
+import org.jenkinsci.plugins.runselector.Messages;
+import org.jenkinsci.plugins.runselector.RunFilter;
+import org.jenkinsci.plugins.runselector.RunSelector;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+
+import javax.annotation.CheckForNull;
+
+/**
+ * The runSelector step selects a specific run from a given project name based on the given selector
+ * and, optionally, the run filter.
+ *
+ * @author Alexandru Somai
+ */
+public class RunSelectorStep extends AbstractStepImpl {
+
+    @CheckForNull
+    private final String projectName;
+
+    private boolean verbose;
+
+    @CheckForNull
+    private RunSelector selector;
+
+    @CheckForNull
+    private RunFilter runFilter;
+
+    @DataBoundConstructor
+    public RunSelectorStep(String projectName) {
+        this.projectName = Util.fixEmptyAndTrim(projectName);
+    }
+
+    @CheckForNull
+    public String getProjectName() {
+        return projectName;
+    }
+
+    @CheckForNull
+    public RunSelector getSelector() {
+        return selector;
+    }
+
+    @DataBoundSetter
+    public void setSelector(RunSelector selector) {
+        this.selector = selector;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    @DataBoundSetter
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    @CheckForNull
+    public RunFilter getRunFilter() {
+        return runFilter;
+    }
+
+    @DataBoundSetter
+    public void setRunFilter(RunFilter runFilter) {
+        this.runFilter = runFilter;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends AbstractStepDescriptorImpl {
+
+        public DescriptorImpl() {
+            super(RunSelectorExecution.class);
+        }
+
+        @Override
+        public String getDisplayName() {
+            return Messages.RunSelectorStep_DisplayName();
+        }
+
+        @Override
+        public String getFunctionName() {
+            return "runSelector";
+        }
+    }
+}
